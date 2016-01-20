c5be202e85a28f16c140e1a750ebb5575466b821
Javileo05 <javier.pomares453@gmail.com>
2016-01-20 14:22:45 -0500

    Blank project created

---
+++ app/src/main/java/com/example/user/nudge/Primary.java
@@ -0,0 +1,13 @@
+package com.example.user.nudge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Primary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary);
+    }
+}
